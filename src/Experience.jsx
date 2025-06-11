3907e2aa3c2395ab3930ecff93081eb8904621e8
Rui Melo <ruimsmelo@gmail.com>
2025-06-11 12:20:54 +0100

    icosahedron

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,17 +2,19 @@ import { OrbitControls } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
+import Player from './Player.jsx'
 
 export default function Experience() {
-    return (
-        <>
-            <OrbitControls makeDefault />
+	return (
+		<>
+			<OrbitControls makeDefault />
 
-            <Physics debug>
-                <Lights />
+			<Physics debug>
+				<Lights />
 
-                <Level />
-            </Physics>
-        </>
-    )
+				<Level />
+				<Player />
+			</Physics>
+		</>
+	)
 }
---
+++ src/Player.jsx
@@ -0,0 +1,8 @@
+export default function Player() {
+	return (
+		<>
+			<icosahedronGeometry args={[0.3, 1]} />
+			<meshStandardMaterial color='mediumpurple' />
+		</>
+	)
+}
