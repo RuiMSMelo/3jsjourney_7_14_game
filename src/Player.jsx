93b3e463a71989f2ad7c25e3413ec2ad18b6fd94
Rui Melo <ruimsmelo@gmail.com>
2025-06-11 12:42:59 +0100

    getKeys

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,14 @@
 import { RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+import { useKeyboardControls } from '@react-three/drei'
 
 export default function Player() {
+	const [subscribeKeys, getKeys] = useKeyboardControls()
+
+	useFrame(() => {
+		const { forward, backward, leftward, rightward } = getKeys()
+	})
+
 	return (
 		<RigidBody
 			canSleep={false}
