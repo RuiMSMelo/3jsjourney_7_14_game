159911d12ac17cf4cdefa881609a0cce47b7e142
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-17 14:16:20 +0100

    Player done

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,19 +1,16 @@
-import { OrbitControls } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
 import Player from './Player.jsx'
 
 export default function Experience() {
-	return (
-		<>
-			<OrbitControls makeDefault />
-
-			<Physics debug>
-				<Lights />
-				<Level />
-				<Player />
-			</Physics>
-		</>
-	)
+    return (
+        <>
+            <Physics debug={false}>
+                <Lights />
+                <Level />
+                <Player />
+            </Physics>
+        </>
+    )
 }
