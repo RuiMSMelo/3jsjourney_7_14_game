abe31e2ca4f875d613a2e3ac9ab563f4b73ff3e1
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-20 14:42:10 +0100

    .time and .restart

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,3 +1,9 @@
 export default function Interface() {
-    return <div className='interface'></div>
+    return (
+        <div className='interface'>
+            {/* Time */}
+            <div className='time'>0.00</div>
+            <div className='restart'>Restart</div>
+        </div>
+    )
 }
